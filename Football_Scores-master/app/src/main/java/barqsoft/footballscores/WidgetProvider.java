cac7af08fecc6b037839822c1961d2de4b9122ae
Pradeep Kiran Chakkirala <Pradeep@Pradeeps-MacBook-Pro-2.local>
2016-01-25 22:55:41 -0500

    Add widget provider class

---
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/WidgetProvider.java
@@ -0,0 +1,34 @@
+package barqsoft.footballscores;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+/**
+ * Created by Pradeep on 1/25/16.
+ */
+public class WidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        final int count = appWidgetIds.length;
+
+        for (int i = 0; i < count; i++) {
+            int widgetId = appWidgetIds[i];
+
+
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
+                    R.layout.layout_widget);
+            remoteViews.setTextViewText(R.id.tvScore, "Hey there");
+
+//            Intent intent = new Intent(context, SimpleWidgetProvider.class);
+//            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+//            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+//            PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
+//                    0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+//            remoteViews.setOnClickPendingIntent(R.id.actionButton, pendingIntent);
+            appWidgetManager.updateAppWidget(widgetId, remoteViews);
+        }
+    }
+}
